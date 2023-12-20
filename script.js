d29285fe09f2d329dd123acee059dd63d18bb9d0
Mohammed Adel <93832294+mu7amed-adell@users.noreply.github.com>
2023-12-20 14:28:50 +0200

    first commit

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 const currentDateSpan = document.getElementById('current-date');
 const daysUntilStartSpan = document.getElementById('days-until-start');
 const daysLeftInServiceSpan = document.getElementById('days-left-in-service');
-const totalDaysLeftSpan = document.getElementById('total-days-left');
+const timeUntilStartSpan = document.getElementById('time-until-start'); // For the detailed countdown to start date
+const serviceTimeCountdownSpan = document.getElementById('service-time-countdown'); // For the detailed service countdown
 
-const startDate = new Date('2024-01-06');
+const startDate = new Date('2024-01-06T00:00:00'); // Start date at midnight
 const serviceDuration = 90 * 24 * 60 * 60 * 1000; // 90 days in milliseconds
-const endDate = new Date(startDate.getTime() + serviceDuration);
+const endDate = new Date(startDate.getTime() + serviceDuration); // End date is 90 days from start date
 
 function updateCountdown() {
     const now = new Date();
     currentDateSpan.textContent = now.toLocaleDateString();
 
-    const timeUntilStart = Math.max(startDate - now, 0);
-    const daysUntilStart = Math.ceil(timeUntilStart / (1000 * 60 * 60 * 24));
-    daysUntilStartSpan.textContent = daysUntilStart;
+    const timeUntilStart = startDate - now;
+    const timeLeftInService = endDate - now;
 
-    const timeLeftInService = Math.max(endDate - now, 0);
-    const daysLeftInService = daysUntilStart > 0 ? serviceDuration / (1000 * 60 * 60 * 24) : Math.ceil(timeLeftInService / (1000 * 60 * 60 * 24));
+    // Update the days until start and in service only if they are positive
+    const daysUntilStart = timeUntilStart > 0 ? Math.floor(timeUntilStart / (1000 * 60 * 60 * 24)) : 0;
+    const daysLeftInService = timeLeftInService > 0 ? Math.floor(timeLeftInService / (1000 * 60 * 60 * 24)) : 0;
+
+    daysUntilStartSpan.textContent = daysUntilStart;
     daysLeftInServiceSpan.textContent = daysLeftInService;
 
-    // Assuming 'total days left' is the sum of days until the start and days left in service
-    const totalDaysLeft = daysUntilStart + daysLeftInService;
-    totalDaysLeftSpan.textContent = totalDaysLeft;
+    // Update the detailed countdown for both periods
+    timeUntilStartSpan.textContent = formatTime(timeUntilStart);
+    serviceTimeCountdownSpan.textContent = formatTime(timeLeftInService);
 }
 
+function formatTime(duration) {
+    if (duration <= 0) {
+        return '0d 0h 0m 0s';
+    }
+    const days = Math.floor(duration / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((duration % (1000 * 60)) / 1000);
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+// Update countdown every second
 setInterval(updateCountdown, 1000);
-updateCountdown();
